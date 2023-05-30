ccb6eedb162959031846cea859cb08389992a070
Andres Oviedo <eaoviedot@gmail.com>
2023-05-30 01:20:33 -0500

    arreglo css, porque no se ve Dropdown

--- index.js
+++ index.js
@@ -75,14 +75,14 @@ function openPage(pageName,elmnt,color) {
 
 // Dropdown
 
-function myFunction() {
+/* function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
-}
+} */
 
 
 // fin del Dropdown
